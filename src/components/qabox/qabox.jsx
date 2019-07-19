9269d22433f4d6667635a9f61b6491c1c31cc4f9
aashankar <aashrit93@gmail.com>
2019-07-20 00:07:44 +0530

    aashrit fe

---
+++ src/components/qabox/qabox.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./qabox.scss";
+
+export default class Qabox extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return (
+      <div class="qabox-container">
+        <div class="question">
+          <div class="profile">asdas</div>
+          <div class="question-details">asdasd</div>
+        </div>
+        <div class="comment">asdasd</div>
+      </div>
+    );
+  }
+}
