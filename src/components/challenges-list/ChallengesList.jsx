0b414364d6af5a8054a568f832924f6196008dc6
aashankar <aashrit93@gmail.com>
2019-07-20 00:45:03 +0530

    rachit changes + aashrit fixes

--- src/components/challenges-list/ChallengesList.jsx
+++ src/components/challenges-list/ChallengesList.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import './ChallengesList.scss';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import ProblemList from '../problem-list/ProblemList';
-import _map from 'lodash/map';
+import React, { Component } from "react";
+import "./ChallengesList.scss";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import ProblemList from "../problem-list/ProblemList";
+import _map from "lodash/map";
 
 export default class ChallengesList extends Component {
   constructor(props) {
     super();
+    this.state = { down: true };
   }
 
   render() {
@@ -18,23 +19,48 @@ export default class ChallengesList extends Component {
             return (
               <Card>
                 <Card.Header>
-                  <Accordion.Toggle as={Card.Header} variant="link" eventKey={index}>
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    variant="link"
+                    eventKey={index}
+                    onClick={() =>
+                      this.setState({
+                        down: false
+                      })
+                    }
+                  >
                     <div className="row no-side-margins">
                       <div className="col-sm-2">
-                        <img src={require('../../assets/chip.png')} className="chip-score-img" alt="chip-logo" />
-                        <span className="count-text">{challenge.problemCount*10}</span>
+                        <img
+                          src={require("../../assets/chip.png")}
+                          className="chip-score-img"
+                          alt="chip-logo"
+                        />
+                        <span className="count-text">
+                          {challenge.problemCount * 10}
+                        </span>
                       </div>
-                      <div className="col-sm-8 challenge-name-text img-center">{challenge.name}</div>
-                      <div className="col-sm-2 img-center" ><i className="up"></i></div>
-                    
+                      <div className="col-sm-8 challenge-name-text ">
+                        <p>{challenge.name}</p>
+                      </div>
+                      <div className="col-sm-2 img-center">
+                        <i
+                          className={this.state.down === true ? "down" : "up"}
+                        />
+                      </div>
                     </div>
-            </Accordion.Toggle>
+                  </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey={index}>
-                  <Card.Body><ProblemList challengeId={challenge._id} problems={challenge.problems} /></Card.Body>
+                  <Card.Body>
+                    <ProblemList
+                      challengeId={challenge._id}
+                      problems={challenge.problems}
+                    />
+                  </Card.Body>
                 </Accordion.Collapse>
               </Card>
-            )
+            );
           })}
         </Accordion>
       </div>
