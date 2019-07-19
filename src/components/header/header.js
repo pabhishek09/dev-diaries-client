81ae83f71120e4b60bcc86793466cbeab5de0bbd
rachit handa <rhanda@deloitte.com>
2019-07-19 19:43:07 +0530

    changes to the profile page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './header.scss';
 
-const HeaderComp = () => (
-    <header className="appHeader row text-center">
-        <div className="col-sm-3">
-            <img src={require('../../assets/dev-diaries-header.png')} className="dev-diaries-logo" alt="dev-diaries-logo" />
-        </div>
-        <div className="col-sm-6">
-        <NavLink to="/profile" exact className="navLinkClass" >My Profile</NavLink>
-        <NavLink to="/playground" exact className="navLinkClass">Playground</NavLink>
-        <NavLink to="/forum" exact className="navLinkClass">Forum</NavLink>
-        </div>
-        <div className="col-sm-3">
-            <img src={require('../../assets/dev-diaries-header.png')} className="user-img" alt="dev-diaries-logo" />
-        </div>
-       
-    </header>
-);
-
-export default HeaderComp;
+export default class HeaderComp extends Component{
+    constructor(props) {
+        super();
+        this.state = {
+            userDetails:props.userDetails
+        }
+    }
+    componentWillReceiveProps(nextProps){
+        if(nextProps.userDetails && (!this.props.userDetails)){
+            this.setState({
+                userDetails:nextProps.userDetails
+            })
+        }
+    }
+    render(){
+        let userDetails=this.state.userDetails;
+        return(
+            <header className="appHeader row ">
+            <div className="col-sm-3 text-center">
+                <img src={require('../../assets/dev-diaries-header.png')} className="dev-diaries-logo" alt="dev-diaries-logo" />
+            </div>
+            <div className="col-sm-6 header-links">
+            <NavLink to="/profile" exact className="navLinkClass" >My Profile</NavLink>
+            <NavLink to="/playground" exact className="navLinkClass">Playground</NavLink>
+            <NavLink to="/forum" exact className="navLinkClass">Forum</NavLink>
+            </div>
+            <div className="col-sm-3 text-center">
+                {userDetails && <img src={userDetails.avatar_url} className="user-img" alt="dev-diaries-logo" />}
+                
+            </div>
+           
+        </header>
+        )
+            
+    }
+}
--- src/views/forum/forum.jsx
+++ src/views/forum/forum.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-
+import UserContext from '../../userDetails';
 import './forum.scss';
 
 export default class ForumComponent extends Component {
+    static contextType = UserContext
     constructor(){
         super();
         this.state={
         }
     }
-
+    componentDidMount() {
+        const user = this.context
+    
+        console.log(user);
+    }
     render() {
         return (
             <div className="center-abs">
