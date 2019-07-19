53e67cea97c503da571eb9ba83b9e4f95b89ddb6
Pandey <abhishekpandey7@deloitte.com>
2019-07-19 20:29:10 +0530

    updated operators

--- src/components/problem-list/ProblemList.jsx
+++ src/components/problem-list/ProblemList.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { NavLink } from 'react-router-dom';
 import './ProblemList.scss';
+import _map from 'lodash/map';
 
 export default class ProblemList extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ export default class ProblemList extends Component {
   render() {
     return (
       <ListGroup>
-      {this.props.problems.map((problem, index) => {
+      {_map(this.props.problems, (problem, index) => {
         return( <ListGroup.Item key={index}>
         <NavLink to={`/playground/${this.props.challengeId}/${problem._id}`}>{problem.name}</NavLink>
         <p>{problem.desc}</p>
